e5394289b79315f069b5fb72dc07893de34e3fee
DhruvaKatta <65651748+dhruvakatta@users.noreply.github.com>
2021-10-23 16:25:47 +0530

    Add files via upload

---
+++ springbootaws-master/src/main/java/com/dhruva/aws/SpringbootawsApplication.java
@@ -0,0 +1,13 @@
+package com.vnjraju.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootawsApplication.class, args);
+    }
+
+}
---
+++ springbootaws-master/src/test/java/com/dhruva/aws/SpringbootawsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vnjraju.aws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootawsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
